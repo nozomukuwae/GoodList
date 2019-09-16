15af8d5d50f55a3ee35725f8828adbff7aebbe0a
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-09-16 20:14:31 +0900

    Initial commit

---
+++ GoodList/Controllers/AddTaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddTaskViewController.swift
+//  GoodList
+//
+//  Created by Nozomu Kuwae on 9/16/19.
+//  Copyright © 2019 Nozomu Kuwae. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+    
+    
+}
---
+++ GoodList/Controllers/TaskListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TaskListViewController.swift
+//  GoodList
+//
+//  Created by Nozomu Kuwae on 9/16/19.
+//  Copyright © 2019 Nozomu Kuwae. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell 
+        
+    }
+    
+}
