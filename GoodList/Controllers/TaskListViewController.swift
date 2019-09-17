f618af60b1c63aeaa8430b76d357d65c32e1d8c6
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-09-17 21:56:04 +0900

    Display tasks

--- GoodList/Controllers/TaskListViewController.swift
+++ GoodList/Controllers/TaskListViewController.swift
@@ -30,12 +30,13 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.filteredTasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        cell.textLabel?.text = self.filteredTasks[indexPath.row].title
         
         return cell 
         
@@ -63,17 +64,23 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
         filterTasks(by: priority)
     }
     
+    private func updateTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
     private func filterTasks(by priority: Priority?) {
         if let priority = priority {
             tasks.map { tasks -> [Task] in
                 return tasks.filter { $0.priority == priority }
                 }.subscribe(onNext: { [weak self] tasks in
                     self?.filteredTasks = tasks
-                    print(tasks)
+                    self?.updateTableView()
                 }).disposed(by: disposeBag)
         } else {
             filteredTasks = tasks.value
-            print(filteredTasks)
+            updateTableView()
         }
     }
 }
